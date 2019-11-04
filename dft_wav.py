5cd1fb747174c2ef56c810b94bfed47d90b6f7d2
Tim Bechtel <bechtelt@physik.hu-berlin.de>
2019-11-04 23:00:44 +0100

    changed function in dft_wav again, now it is the right one, added time scaling test to testing_sketch

--- dft_wav.py
+++ dft_wav.py
@@ -4,40 +4,38 @@ from scipy.io import wavfile
 
 def dft(file, rate=1):
     # discrete fourier transform
-    if type(file) == str:
+    # 'file' input takes filename of a .wav file or 1-D input vector
+    if type(file) == str: # tests the input format
         assert file.endswith(".wav"), "file is not a wavefile"
         rate, v = wavfile.read(file)
     else:
         v = file
     assert len(np.shape(v)) == 1, "input array needs to be one-dimensional"
-    N = len(v)
+    N = len(v) # get length of vector
     xt = np.arange(0,N)*2*np.pi*rate/N # frequency vector in hertz
+    vt = np.zeros(N,dtype="complex_") # initialize array
+    # we only want to calculate the exponent N-times, evenly spaced along the unit circle
+    # for this a vector of 0 to N-1 integers is generated
     n_vec = np.arange(N)
-    n_mat, k_mat = np.meshgrid(np.arange(N),np.arange(N))
-    vt = np.zeros(N,dtype="complex_")
-    exp_mat = np.zeros((N,N),dtype="complex_")
+    # then the exponential is evaluated at those points
     exp_vec = np.exp(-1j*2*np.pi/N*n_vec)
-    ind_mat = np.mod(n_mat*k_mat, N, dtype=np.intp)
-    exp_mat = exp_vec[ind_mat]
-    vt = np.matmul(exp_mat,v)
+    for k in np.arange(N):
+        # now the exp_vec needs to be called at the right indices, this is done at n*k mod N
+        vt[k] = np.dot(exp_vec[np.mod(n_vec*k,N, dtype=np.intp)],v)
     return xt, vt
 
-def idft(file, rate=1):
+def idft(v):
     # inverse discrete fourier transform
-    if type(file) == str:
-        assert file.endswith(".wav"), "file is not a wavefile"
-        rate, v = wavfile.read(file)
-    else:
-        v = file
+    # 'v' is the input vector on which the inverse fourier transform is performed
+    # 'v' needs to be a 1-D array
+    # for comments see the dft function, which is very similar
     assert len(np.shape(v)) == 1, "input array needs to be one-dimensional"
     N = len(v)
-    xt = np.arange(0,N)*2*np.pi*rate/N # scaling may need to be changed
-    n_vec = np.arange(N)
-    n_mat, k_mat = np.meshgrid(np.arange(N),np.arange(N))
     vt = np.zeros(N,dtype="complex_")
-    exp_mat = np.zeros((N,N),dtype="complex_")
-    exp_vec = np.exp(1j*2*np.pi/N*n_vec)
-    ind_mat = np.mod(n_mat*k_mat, N, dtype=np.intp)
-    exp_mat = exp_vec[ind_mat]
-    vt = 1/N*np.matmul(exp_mat,v)
+    n_vec = np.arange(N)
+    exp_vec = np.exp(1j*2*np.pi/N*n_vec) # notice the missing (-) in the argument
+    # this makes this the inverse function
+    for k in np.arange(N):
+        # normalization by 1/N cannot be missing:
+        vt[k] = 1/N*np.dot(exp_vec[np.mod(n_vec*k,N, dtype=np.intp)],v)
     return vt
